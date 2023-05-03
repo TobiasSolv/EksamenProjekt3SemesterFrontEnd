c36413bb15c1668e0470aadafc4d645d4c85644a
TobiasSolv <tobiasurban@hotmail.com>
2023-05-03 14:11:25 +0200

    Added button effect

--- Js/Homepage.js
+++ Js/Homepage.js
@@ -1,16 +1,22 @@
 var inputval = document.querySelector('#cityinput')
-var btn = document.querySelector('#add');
+var button = document.querySelector('#add');
 var city = document.querySelector('#cityoutput')
 var descrip = document.querySelector('#description')
 var temp = document.querySelector('#temp')
 var wind = document.querySelector('#wind')
 apik = "3045dd712ffe6e702e3245525ac7fa38"
-function convertion(val)
+
+function convertionTemp(temp)
 {
-    return (val - 273).toFixed(2)
+    return (temp - 273).toFixed(2)
 }
 
-btn.addEventListener('click', function()
+function convertionWind(wind)
+{
+    return (wind / 0.28).toFixed(2)
+}
+
+button.addEventListener('click', function()
 {
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid='+apik)
         .then(res => res.json())
@@ -23,9 +29,9 @@ btn.addEventListener('click', function()
             var tempature = data['main']['temp']
             var wndspd = data['wind']['speed']
             city.innerHTML=`Weather of <span>${nameval}<span>`
-            temp.innerHTML = `Temperature: <span>${ convertion(tempature)} C</span>`
+            temp.innerHTML = `Temperature: <span>${ convertionTemp(tempature)} C</span>`
             description.innerHTML = `Sky Conditions: <span>${descrip}<span>`
-            wind.innerHTML = `Wind Speed: <span>${wndspd} km/h<span>`
+            wind.innerHTML = `Wind Speed: <span>${convertionWind(wndspd)} m/s<span>`
 
         })
 
