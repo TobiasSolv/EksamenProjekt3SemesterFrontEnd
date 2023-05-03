4c107831ff639a660855ab31f3b6eecd7706cee3
TobiasSolv <tobiasurban@hotmail.com>
2023-05-03 14:56:37 +0200

    Added icon to temp and wind

--- Js/Homepage.js
+++ Js/Homepage.js
@@ -8,16 +8,48 @@ apik = "3045dd712ffe6e702e3245525ac7fa38"
 
 function convertionTemp(temp)
 {
-    return (temp - 273).toFixed(2)
+    const celsius = (temp - 273).toFixed(2);
+    let icon;
+    if (celsius > 30) {
+        icon = '<i class="fas fa-thermometer-full"></i>';
+    } else if (celsius > 20) {
+        icon = '<i class="fas fa-thermometer-three-quarters"></i>';
+    } else if (celsius > 10) {
+        icon = '<i class="fas fa-thermometer-half"></i>';
+    } else if (celsius > 0) {
+        icon = '<i class="fas fa-thermometer-quarter"></i>';
+    } else {
+        icon = '<i class="fas fa-thermometer-empty"></i>';
+    }
+    return [celsius, icon];
 }
 
 function convertionWind(wind)
 {
-    return (wind / 0.28).toFixed(2)
+    const speed = (wind).toFixed(2);
+    let icon;
+    if (speed > 30) {
+        icon = '<i class="fas fa-wind"></i>';
+    } else if (speed > 20) {
+        icon = '<i class="fas fa-wind"></i>';
+    } else if (speed > 10) {
+        icon = '<i class="fas fa-wind"></i>';
+    } else if (speed > 5) {
+        icon = '<i class="fas fa-wind"></i>';
+    } else {
+        icon = '<i class="fas fa-wind"></i>';
+    }
+    return [speed, icon];
 }
 
 button.addEventListener('click', function()
 {
+    fetch(
+        'https://api.openweathermap.org/data/2.5/weather?q=' +
+        inputval.value +
+        '&appid=' +
+        apik
+    )
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid='+apik)
         .then(res => res.json())
 
@@ -29,7 +61,7 @@ button.addEventListener('click', function()
             var tempature = data['main']['temp']
             var wndspd = data['wind']['speed']
             city.innerHTML=`Weather of <span>${nameval}<span>`
-            temp.innerHTML = `Temperature: <span>${ convertionTemp(tempature)} °C</span>`
+            temp.innerHTML = `Temperature: <span> ${ convertionTemp(tempature)}°C</span>`
             description.innerHTML = `Sky Conditions: <span>${descrip}<span>`
             wind.innerHTML = `Wind Speed: <span>${convertionWind(wndspd)} m/s<span>`
 
